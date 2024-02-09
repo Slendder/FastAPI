f9e0af08f74dc08b3eb9665fedd0e32f842fede9
Slendder <141583251+Slendder@users.noreply.github.com>
2024-02-08 22:56:44 -0300

    as

--- Python-Intermediate/00-Dates.py
+++ Python-Intermediate/00-Dates.py
@@ -1,3 +1,24 @@
 ### Dates ###
-import datetime
+from datetime import datetime
 
+now = datetime.now()
+
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.min)
+    print(date.second)
+    print(now.timestamp())
+
+
+print(now)
+
+year_2023 = datetime(2023, 1, 1)
+
+if input("asda: ") != "adsa":
+    print('asd')
+
+print_date(year_2023)
