d46f2be3409162049daccfd87ffb7c47434ed936
Bautista Prieto <bautista.prieto.hoses@gmail.com>
2024-03-17 19:22:09 -0300

    w

--- 100-days/07.py
+++
@@ -1,9 +0,0 @@
-"""
-* Crea un programa que invierta el orden de una cadena de texto
-* sin usar funciones propias del lenguaje que lo hagan de forma automática.
-* - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
-"""
-
-def reverse(obj):
-    return obj[::-1]
-
